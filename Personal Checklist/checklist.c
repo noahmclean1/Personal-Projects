54134444f85014d9660b36fdca265619b49b0a70
Noah McLean <noahdmclean@gmail.com>
2017-06-21 17:58:55 -0700

    Initial add

---
+++ Personal Checklist/checklist.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <checklist.h>
+
+int main(int argc, char const *argv[])
+{
+	// Checklist execution function
+
+	/* If no args, start a new list */
+	/* Function to list checklists */
+	/* If arg, load list */
+	/* Functions for new item, check item, delete item, display full list, display unchecked list */
+
+	/* code */
+	return 0;
+}
